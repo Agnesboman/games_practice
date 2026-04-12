d1136f0e9eb11fe5a22f910df97a66cf170eafce
Agnesboman <agnes-boman@hotmail.com>
2026-04-12 02:59:03 +0200

    Add: render.py added text for first view, added button, does not work yet. notes.txt: added a link

--- Snake/render.py
+++ Snake/render.py
@@ -1,4 +1,6 @@
 import sys
+from PySide6 import QtWidgets, QtCore
+import random
 from PySide6.QtWidgets import (
     QGraphicsScene, QGraphicsView,
     QGraphicsRectItem, QApplication
@@ -8,6 +10,7 @@ from PySide6.QtCore import Qt
 from shapeFactory import shape
 
 
+
 class View:
     def __init__(self, height, width):
         self.height = height
@@ -15,16 +18,29 @@ class View:
         self.app = QApplication(sys.argv)
         self.scene = QGraphicsScene(0, 0, self.width, self.height)
         self.view = QGraphicsView(self.scene)
+        
 
     def startView(self):
         #graphics
         rect_obj = shape(200, 50, 0, 0)
         rect = rect_obj.rectangle(Qt.red, Qt.black, 3)
-
+        self.button = QtWidgets.QPushButton("Click me biiaatch")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+        
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
         self.scene.addItem(rect)
+        self.button.clicked.connect(self.magic)
+
         self.view.show()
 
         return self.app.exec()
+        
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
 
     def gameView(self):
         self.view.show()
